a41d4bf9b7c5510d45df126d00bbe9d80a57a48d
MentosNoMint <dima-krut-2020@bk.ru>
2024-12-26 12:18:34 +0500

    add strapi

---
+++ src/api/savekid/controllers/savekid.ts
@@ -0,0 +1,7 @@
+/**
+ * savekid controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::savekid.savekid');
---
+++ src/api/savekid/services/savekid.ts
@@ -0,0 +1,7 @@
+/**
+ * savekid service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::savekid.savekid');
